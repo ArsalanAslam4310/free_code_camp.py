264df7924448598747f43809d09be213a96ae928
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-01 11:25:16 +0500

    Create title_case.py

---
+++ title_case.py
@@ -0,0 +1,10 @@
+def title_case(string):
+    string=string.lower()
+    for i in range(len(string)):
+        if string[i] !=" ":
+            string[i].upper()
+    return string
+
+string="I'm a little tea pot"
+print(title_case(string))
+
