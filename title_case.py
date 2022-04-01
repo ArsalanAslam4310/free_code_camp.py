f13a2e1cca55455fa8d937d65b2f33d9581effcf
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-01 23:00:01 +0500

    Update title_case.py

--- title_case.py
+++ title_case.py
@@ -1,8 +1,8 @@
 def title_case(string):
     string=string.lower()
     for i in range(len(string)):
-        if string[i] !=" ":
-            string[i].upper()
+        if string[i] == " ":
+            string[i+1].upper()
     return string
 
 string="I'm a little tea pot"
