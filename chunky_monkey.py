ec89434176f321d6e93ca544cea4e904e982975c
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-05 10:58:37 +0500

    Update chunky_monkey.py

--- chunky_monkey.py
+++ chunky_monkey.py
@@ -1,10 +1,17 @@
 def chunkey(lis,size):
-    new=[]
-    for i in range(size):
-        if len(lis)//size:
-            new.append(size)
-    return new
+    inner_list=[]
+    outer_list=[]
 
+    idx=0
+    for _ in range(size):
+        i=0
+        while i<len(lis)//size:
+            inner_list.append(lis[idx])
+            idx+=1
+            i+=1
+        outer_list.append(inner_list)
+        inner_list=[]
+    return outer_list
 
 lis=[6,6,54,3,22,2]
 print(chunkey(lis,3))
