4a234274e5664737d1b9520223f05ef84fe2381a
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-06 22:33:19 +0500

    Create sum_all_numbers_in_range.py

---
+++ sum_all_numbers_in_range.py
@@ -0,0 +1,13 @@
+def sum_all(lis):
+    '''
+    sum all number in range of list value 
+    '''
+    sum = 0
+    
+    for i in range(lis[0],lis[1]):
+        sum = sum +i
+
+    return sum 
+
+lis=[5,10]
+print(sum_all(lis))
