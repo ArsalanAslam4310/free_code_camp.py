f9c0d2294df6b681d2ca5307649272f8a5288b18
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-08 10:33:09 +0500

    Update difference_of_two_arrays.py

--- difference_of_two_arrays.py
+++ difference_of_two_arrays.py
@@ -4,11 +4,13 @@ def diff_array(lis1,lis2):
     '''
     new=[]
 
-    for i in range(len(lis1)):
+    for val in lis1:
         if lis1[i] not in lis2:
             new.append(lis1[i])
-         if lis2[i] not in lis1
+     for val in lis2:
+        if val not in lis1
             new.append(lis2)
+            
     return new
 
 
