e133bfee38c8f38b106b13f928c605d363cf4709
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-19 14:32:55 +0500

    Update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -3,6 +3,7 @@ def pig_latin(string):
     for i in range(len(string)):
         if string[0]=="b,c,d,f,g,h,j,k,l,m,n,p,q,r,t,v,w,x,y,z":
             string+= string[0]+"ay"
+            string=string.pop(0)
         else:
             string+= string[0]+ "way"
             string=string.pop(0)
