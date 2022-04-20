dd1b3eb14165de4d8f2f15c4b16d4316b5ace2ee
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-20 10:34:58 +0500

    Update search_and_replace.py

--- search_and_replace.py
+++ search_and_replace.py
@@ -1,11 +1,15 @@
-def search_and_replace(string,exist,new_occurence):
+def search_and_replace(string, before, after):
     '''
     search and replace the value
     '''
-    for i in range(len(new_occurence)):
-        new_occurence[0]=new_occurence[0].upper()
-        x=string.replace(exist,new_occurence)
-    return x
+    after = list(after)
+    if before[0].isupper():
+        after[0] = after[0].upper()
+    else:
+        after[0] = after[0].lower()
+    after = ''.join(after)
 
-string="Let us get back to more coding"
-print(search_and_replace(string,"coding","algorithms"))
+    return string.replace(before, after)
+
+
+print(search_and_replace("Let us get back to more Coding", "Coding", "algorithms"))
