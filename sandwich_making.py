12b3dbdfac9d1b0737bf062bfe6e231ee9580a09
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-23 10:54:42 +0500

    Create sandwich_making.py

---
+++ sandwich_making.py
@@ -0,0 +1,20 @@
+while True:
+    print("Enter a bread type of Wheat,White or sourdough")
+    bread = input()
+    print("Enter a protein type of chicken,turkey,ham or tofu")
+    protein=input()
+    print("Yes or No for cheese")
+    cheese=input()
+    print("cheese type cheddar, swiss,or mozzarella")
+    chees=input()
+    print("yes or no for mayo,mustard,lettuce, or tomato")
+    mayo=input()
+    
+    print("how many sandwiches they want")
+    sandwiches_total=input()
+    if True:
+        sandwich=0
+        if sandwich == 100:
+            print(sandwiches_total*sandwich)
+            break
+print("Done")
