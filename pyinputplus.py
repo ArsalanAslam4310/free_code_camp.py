7db2208fe27cc1eb3c99b92c35256e1352079466
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-23 12:37:01 +0500

    Update pyinputplus.py

--- pyinputplus.py
+++ pyinputplus.py
@@ -1,6 +1,30 @@
-import pyinputplus as py
+import time
+import random
+import pyinputplus as pyip
 
-response=py.inputNum(limit=3,default='N/A')
-response=py.inputNum(timeout=10,default='incorrect')
+numberOfQuestions = 10
+correctAnswers = 0
 
-print(response)
+for questionNumber in range(numberOfQuestions):
+    # Pick two random numbers:
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+    prompt = '#%s: %s x %s = ' % (questionNumber, num1, num2)
+    try:
+        # Right answers are handled by allowRegexes.
+        # Wrong answers are handled by blockRegexes, with a custom message.
+        pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
+                      blockRegexes=[('.*', 'Incorrect!')],
+                      timeout=8, limit=3)
+    except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+
+    else:
+        # This block runs if no exceptions were raised in the try block.
+        print('Correct!')
+        correctAnswers += 1
+        time.sleep(1)  # Brief pause to let user see the result.
+
+print('Score: %s / %s' % (correctAnswers, numberOfQuestions))
