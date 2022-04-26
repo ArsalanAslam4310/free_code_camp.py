6c58a9712aac24c2226b362452e2e83cc6fcf6cd
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-04-26 10:54:08 +0500

    Create sum_fibonacci_number.py

---
+++ sum_fibonacci_number.py
@@ -0,0 +1,18 @@
+def fibonachi_number(num):
+    '''
+    sum fibonacci numbers
+    '''
+    prev_num=1
+    curr_num=1
+    next_num = prev_num+curr_num
+    sum =0
+
+    while next_num<=num:
+        prev_num=curr_num
+        curr_num=next_num
+        next_num=prev_num+curr_num
+        if curr_num %2!=0:
+            sum += curr_num
+    return sum 
+
+print(fibonachi_number(1000))
